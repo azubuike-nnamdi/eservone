9d1d1e20f4d891a830695f33507c6f88fe98f48a
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-18 15:30:47 +0100

    chore: include forgot password :zap:

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,4 +1,5 @@
 const SIGN_IN = "/auth/sign-in";
 const SIGN_UP = "/auth/sign-up";
+const FORGOT_PASSWORD = "/auth/forgot-password";
 
-export { SIGN_IN, SIGN_UP };
+export { SIGN_IN, SIGN_UP, FORGOT_PASSWORD };
