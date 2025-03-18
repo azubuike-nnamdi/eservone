de345577beb3cc8ebcd81bf5b576454f25193518
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-18 12:25:16 +0100

    chore: import onboarding component :zap:

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,6 @@
-import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import OnboardingFlatList from "@/components/onboarding/onboarding-flat-list";
+
 
 export default function Index() {
-  return (
-    <SafeAreaView className="flex justify-center items-center">
-      <Image source={images.logo} className="w-20 h-20" />
-    </SafeAreaView>
-  );
+  return <OnboardingFlatList />
 }
