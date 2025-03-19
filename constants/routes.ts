87e4bd4b76941e8507f8535a6b5243f77c977feb
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-19 11:09:06 +0100

    chore: add new route :zap:

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,5 +1,10 @@
 const SIGN_IN = "/auth/sign-in";
 const SIGN_UP = "/auth/sign-up";
 const FORGOT_PASSWORD = "/auth/forgot-password";
+const VERIFY_EMAIL = "/auth/verify-email";
+const RESET_PASSWORD = "/auth/reset-password";
+const CONTINUE_SIGN_UP = "/auth/continue-sign-up";
+const PROFILE = '/(tabs)/profile';
+const HOME = '/(root)/(tabs)';
 
-export { SIGN_IN, SIGN_UP, FORGOT_PASSWORD };
+export { SIGN_IN, SIGN_UP, FORGOT_PASSWORD, VERIFY_EMAIL, RESET_PASSWORD, CONTINUE_SIGN_UP, PROFILE, HOME };
