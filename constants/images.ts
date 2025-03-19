1e582ecf3e54cd3b807c6b063fae497f652fe1cf
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-19 11:08:00 +0100

    chore: export iimages :zap:

--- constants/images.ts
+++ constants/images.ts
@@ -1,7 +1,18 @@
 import logo from "@/assets/images/eservone-logo.png";
 import slideImage1 from "@/assets/images/slider-image-1.png";
-
+import slideImage2 from "@/assets/images/slider-image-2.png";
+import slideImage3 from "@/assets/images/slider-image-3.png";
+import slideImage4 from "@/assets/images/slider-image-4.png";
+import slideImage5 from "@/assets/images/slider-image-5.png";
+import SendEmail from "@/assets/images/Send.png";
+import Terms from "@/assets/images/Paper.png";
 export default {
   logo,
   slideImage1,
+  slideImage2,
+  slideImage3,
+  slideImage4,
+  slideImage5,
+  SendEmail,
+  Terms,
 };
