129c5dedc06e209a08a8f65669b1938eed03247b
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-21 14:19:26 +0100

    chore: sign-in page :fire:

--- app/(root)/auth/sign-in.tsx
+++ app/(root)/auth/sign-in.tsx
@@ -5,20 +5,39 @@ import { router } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import AuthHeader from '@/components/common/auth-header'
 import Button from '@/components/common/button'
-import { FORGOT_PASSWORD, SIGN_UP } from '@/constants/routes'
+import { FORGOT_PASSWORD, HOME, SIGN_UP } from '@/constants/routes'
+import { validateEmail } from '@/lib/helpler'
 
 export default function SignIn() {
   const [email, setEmail] = useState("")
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
+
   const [isLoading, setIsLoading] = useState(false)
 
+
+  const handleEmailChange = (text: string) => {
+    setEmail(text);
+    setIsValidEmail(true);
+    setErrorMessage("");
+  };
   const handleSignIn = () => {
+    const isValid = validateEmail(email);
+    setIsValidEmail(isValid);
+
+    if (!isValid) {
+      setErrorMessage("Please enter a valid email address");
+      return;
+    }
+
     if (email && password) {
       setIsLoading(true)
       // Simulate API call
       setTimeout(() => {
         console.log("Signing in with:", email, password)
+        router.push(HOME)
         setIsLoading(false)
         // Navigate to home or dashboard after successful login
       }, 2000)
@@ -57,12 +76,17 @@ export default function SignIn() {
               placeholder="example@gmail.com"
               placeholderTextColor="#CCCCCC"
               value={email}
-              onChangeText={setEmail}
+              onChangeText={handleEmailChange}
               keyboardType="email-address"
               autoCapitalize="none"
               textAlign="left"
             />
 
+            {!isValidEmail && (
+              <Text className="text-danger text-sm mb-2 text-center">
+                {errorMessage}
+              </Text>
+            )}
             {/* Password Field */}
             <Text className="text-base mb-2 text-black">
               Password
