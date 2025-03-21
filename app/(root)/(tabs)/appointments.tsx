24b22072bf01125f15ee9aa712518237044aa11a
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-21 09:03:09 +0100

    feat: appointment page :zap:

---
+++ app/(root)/(tabs)/appointments.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+export default function Appointments() {
+  return (
+    <SafeAreaView>
+      <Text>Appointments</Text>
+    </SafeAreaView>
+  )
+}
