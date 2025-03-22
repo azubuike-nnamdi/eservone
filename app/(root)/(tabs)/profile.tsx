fb925bab31b564da9f1d9d4345bd5f366cab8568
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-22 19:39:22 +0100

    chore: implement sign out :zap:

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Text, ScrollView, View, Image, Alert } from 'react-native'
+import { Text, ScrollView, View, Image, Alert, ActivityIndicator } from 'react-native'
 import icons from '@/constants/icons'
 import ProfileHeader from '@/components/common/profile-header'
 import GeneralSetting from '@/components/profile/general-setting'
 import { router } from 'expo-router'
 import { MANAGE_SERVICES, BUSINESS_PROFILE, EARNINGS, CERTIFICATES, SIGN_IN } from '@/constants/routes'
 import { generalSettings, supportSettings, legalSettings } from '@/constants/data'
+import { useAuth } from '@/context/auth-context'
+import { useUser } from '@/context/user-context'
 
 export default function Profile() {
+  const { clearToken, isLoading: isAuthLoading } = useAuth()
+  const { clearUser, isLoading: isUserLoading } = useUser()
+
   const handleSignOut = async () => {
-    setTimeout(() => {
+    try {
+      await clearToken()
+      await clearUser()
       router.push(SIGN_IN)
-      Alert.alert('Success', 'Logged out successfully');
-    }, 2000)
+    } catch (error) {
+      Alert.alert('Error', 'Failed to sign out')
+    }
+  }
+
+  if (isAuthLoading || isUserLoading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-white">
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text className="mt-2 font-rubikMedium">Signing out...</Text>
+      </View>
+    )
   }
 
   return (
