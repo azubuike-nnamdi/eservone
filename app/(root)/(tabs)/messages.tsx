e5b085dd160f105c522022eb8718bb736809c7a7
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-27 19:39:17 +0100

    feat: messages routes :racehorse:

--- app/(root)/(tabs)/messages.tsx
+++ app/(root)/(tabs)/messages.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import ProfileHeader from "@/components/common/profile-header";
+import { ScrollView } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Messages() {
   return (
-    <View>
-      <Text>Messages</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName='pb-32 px-7'>
+        <ProfileHeader title='Messages' showNotification={false} />
+      </ScrollView>
+    </SafeAreaView>
   )
 } 
