5af239987ae5d5a0bac36fd80b964c8823a24b5f
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-27 19:41:15 +0100

    chore: include greetings :zap:

--- lib/helpler.ts
+++ lib/helpler.ts
@@ -3,7 +3,18 @@ const validateEmail = (text: string) => {
   return emailRegex.test(text);
 };
 
+const getGreeting = (): string => {
+  const hour = new Date().getHours();
 
+  if (hour >= 5 && hour < 12) {
+    return 'Good morning';
+  } else if (hour >= 12 && hour < 17) {
+    return 'Good afternoon';
+  } else if (hour >= 17 && hour < 21) {
+    return 'Good evening';
+  } else {
+    return 'Good night';
+  }
+};
 
-
-export { validateEmail };
+export { validateEmail, getGreeting };
