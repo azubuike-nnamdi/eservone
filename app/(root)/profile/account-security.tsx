a9e8751c96273040eee1dd8a563c79d43246c36a
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-28 15:40:10 +0100

    chore: implement change password flow :zap:

--- app/(root)/profile/account-security.tsx
+++ app/(root)/profile/account-security.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { ScrollView } from 'react-native'
+import { ScrollView, View, Text } from 'react-native'
 import ProfileHeader from '@/components/common/profile-header'
-
+import ProfileChangePassword from '@/components/profile/profile-change-password'
 export default function AccountSecurity() {
   return (
     <SafeAreaView className='flex-1 bg-white'>
       <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName='pb-32 px-7'>
         <ProfileHeader title='Account security' showNotification={false} />
+        <View className="border-b border-gray-100 my-3" />
+
+        <View className='flex-1 justify-center items-center'>
+          <Text className='text-black/50 text-center mx-auto text-2xl'>For added security, we recommend periodically
+            reviewing your account activity. If you notice
+            anything suspicious, please change your
+            password immediately.</Text>
+        </View>
+
+        <View>
+          <ProfileChangePassword />
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
