9e33d6091e1747b513c30dc4a416cfbd9d0ea26a
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-07 23:04:20 +0100

    chore:  create a layout for all profile related route :racehorse:

---
+++ app/(root)/profile/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+
+export default function ProfileLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="account-preference" />
+      <Stack.Screen name="account-security" />
+      <Stack.Screen name="business-profile" />
+      <Stack.Screen name="certificates" />
+      <Stack.Screen name="earnings" />
+      <Stack.Screen name="manage-services" />
+      <Stack.Screen name="privacy-policy" />
+      <Stack.Screen name="profile-information" />
+      <Stack.Screen name="support" />
+      <Stack.Screen name="terms-of-services" />
+    </Stack>
+  )
+}
