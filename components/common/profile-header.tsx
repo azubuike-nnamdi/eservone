8bbf7e4868059bddea4bd2929d06bdf90c89e4e5
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-07 23:07:52 +0100

    refactor: profile header :hammer:

--- components/common/profile-header.tsx
+++ components/common/profile-header.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
 import icons from '@/constants/icons'
 import { ProfileHeaderProps } from '@/constants/types'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 
 export default function ProfileHeader({
@@ -24,23 +24,26 @@ export default function ProfileHeader({
   }
 
   return (
-    <View className='flex flex-row justify-between items-center mt-7 '>
-      <View className='flex flex-row gap-3 items-center'>
-        {showBackArrow && (
-          <TouchableOpacity onPress={handleBackPress}>
-            <Image source={icons.backArrow} className='size-8' />
-          </TouchableOpacity>
-        )}
-        <Text className='text-xl font-bold text-black-300'>{title}</Text>
-      </View>
+    <View>
+      <View className='flex flex-row justify-between items-center mt-7 px-7'>
+        <View className='flex flex-row gap-3 items-center'>
+          {showBackArrow && (
+            <TouchableOpacity onPress={handleBackPress}>
+              <Image source={icons.backArrow2} className='size-5' />
+            </TouchableOpacity>
+          )}
+          <Text className='text-xl font-bold text-black-300'>{title}</Text>
+        </View>
 
-      {rightComponent || (
-        showNotification && (
-          <TouchableOpacity onPress={onNotificationPress}>
-            <Image source={icons.bell} className='size-5' />
-          </TouchableOpacity>
-        )
-      )}
+        {rightComponent || (
+          showNotification && (
+            <TouchableOpacity onPress={onNotificationPress}>
+              <Image source={icons.bell} className='size-5' />
+            </TouchableOpacity>
+          )
+        )}
+      </View>
+      <View className='border-b border-gray-100 mt-4' />
     </View>
   )
 }
