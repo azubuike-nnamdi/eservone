e499e850183981c1908b12d09f9cf5a25b4c7057
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-07 23:09:07 +0100

    feat: hook to get all services :fire:

---
+++ hooks/query/useGetAllServices.ts
@@ -0,0 +1,23 @@
+//query to get all services
+
+import api from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetAllServices = () => {
+  const getAllServices = async () => {
+    const response = await api.get('/eserve-one/get-service')
+    return response.data
+  }
+  const { data, isPending, error } = useQuery({
+    queryKey: ['services'],
+    queryFn: getAllServices
+  })
+
+  return {
+    data,
+    isPending,
+    error
+  }
+}
+
+export default useGetAllServices
