d79885d9422bdf25b0501db349b1e54b14e33d82
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-27 18:30:25 +0100

    feat: hook to get service by Id :pizza:

---
+++ hooks/query/useGetServiceById.ts
@@ -0,0 +1,21 @@
+import api from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+const useGetServiceById = (serviceId: string) => {
+  const getServiceById = async () => {
+    const response = await api.get(`/eserve-one/get-service-by-id`, {
+      headers: {
+        'id': serviceId
+      }
+    });
+    return response.data;
+  };
+  const { data, isPending, error } = useQuery({
+    queryKey: ["service", serviceId],
+    queryFn: getServiceById,
+  });
+
+  return { data, isPending, error };
+}
+
+export default useGetServiceById;
