7df954b2607fb204c37aca24f19b4d95b45a4956
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-28 07:34:44 +0100

    chore: user services :zap:

--- hooks/query/useGetServicesByUserId.ts
+++ hooks/query/useGetServicesByUserId.ts
@@ -1,22 +1,24 @@
 import api from "@/lib/api";
+import { useAuthStore } from "@/store/auth-store";
 import { useQuery } from "@tanstack/react-query";
 
-const useGetServicesByUserId = (userId: string) => {
+const useGetServicesByUserId = () => {
+  const { accessToken } = useAuthStore();
   const getServicesByUserId = async () => {
     const response = await api.get(`/eserve-one/get-service-by-user-id`, {
       headers: {
-        'userId': userId
+        'Authorization': `${accessToken}`,
       }
     });
     return response.data;
   };
 
-  const { data, isPending, error } = useQuery({
-    queryKey: ["services", userId],
+  const { data, isPending, error, isError } = useQuery({
+    queryKey: ["user-services"],
     queryFn: getServicesByUserId,
   });
 
-  return { data, isPending, error };
+  return { data, isPending, error, isError };
 };
 
 export default useGetServicesByUserId;
