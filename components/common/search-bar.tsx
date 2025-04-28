2679f729bbd1e96495afe0a4a7651a8572746db4
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-28 07:33:15 +0100

    feat: search bar :pizza:

---
+++ components/common/search-bar.tsx
@@ -0,0 +1,37 @@
+import icons from "@/constants/icons";
+import { Image, TextInput, TouchableOpacity } from "react-native";
+
+interface Props {
+  placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      className="flex-row items-center bg-primary-100 rounded-full px-5 py-4"
+    >
+      <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode="contain"
+        tintColor="#000"
+      />
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        className="flex-1 ml-2 text-black-300"
+        placeholderTextColor="#0F0F0"
+        editable={!!onChangeText}
+        pointerEvents={onPress ? "none" : "auto"}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default SearchBar;
