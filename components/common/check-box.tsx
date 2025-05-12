f56a3fa726779d56a133589e8771d3a22e1ccc2b
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-05-12 15:22:56 +0100

    chore: checkbox :zap:

--- components/common/check-box.tsx
+++ components/common/check-box.tsx
@@ -33,8 +33,8 @@ export default function Checkbox({
           <Ionicons
             name="checkmark"
             size={16}
-            color="white"
-            style={{ marginTop: 1 }}
+            color="black"
+            className='font-bold'
           />
         )}
       </View>
