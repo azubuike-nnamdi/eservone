8bfdcf7c9b1aae09e141a61160f610d853d498e9
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-05-14 19:07:54 +0100

    chore: messages with dummy data :zap:

--- app/(root)/(tabs)/messages.tsx
+++ app/(root)/(tabs)/messages.tsx
@@ -1,17 +1,35 @@
 import ProfileHeader from "@/components/common/profile-header";
-import { ScrollView, Text, View } from "react-native";
-
+import { RoomItem } from "@/components/messages/room-item";
+import { chatRooms } from "@/constants/data";
+import { useRouter } from "expo-router";
+import React from "react";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+
 export default function Messages() {
+  const router = useRouter();
+
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName='pb-32 '>
-        <ProfileHeader title='Messages' showNotification={false} />
-        <View className='flex-1 justify-center items-center'>
-          <Text>No messages yet</Text>
-        </View>
-      </ScrollView>
+      <ProfileHeader title='Messages' showNotification=
+        {false} />
+      <FlatList
+        data={chatRooms}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <RoomItem
+            room={item}
+            onPress={() => router.push(`/message-room/${item.id}`)}
+          />
+        )}
+        contentContainerStyle={{ paddingTop: 8 }}
+        ListEmptyComponent={
+          <View className="flex-1 justify-center items-center mt-20">
+            <Text>No chat rooms yet</Text>
+          </View>
+        }
+      />
     </SafeAreaView>
-  )
+  );
 } 
