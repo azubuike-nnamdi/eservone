a75f2d869f89e8046c310cd8f27534644ff8e802
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-09 11:25:00 +0100

    chore: update toast :zap:

--- hooks/mutation/verify-email.ts
+++ hooks/mutation/verify-email.ts
@@ -1,5 +1,6 @@
 import { CONTINUE_SIGN_UP } from "@/constants/routes"
 import type { VerificationPayload } from "@/constants/types"
+import { useToast } from "@/context/toast-context"
 import api from "@/lib/api"
 import { useSignupStore } from "@/store/signup-store"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
@@ -8,15 +9,16 @@ import { router } from "expo-router"
 const ValidateEmail = () => {
   const queryClient = useQueryClient()
   const setJwtToken = useSignupStore((state) => state.setJwtToken)
+  const { showToast } = useToast()
 
   const mutation = useMutation({
     mutationFn: (payload: VerificationPayload) => {
       return api.post(`/eserve-one/verify-email`, payload)
     },
     onSuccess: ({ data }) => {
-      console.log("Success response:", data?.data)
       if (data) {
-        console.log("Storing JWT Token:", data?.data?.jwtToken);
+        // console.log("Storing JWT Token:", data?.data?.jwtToken);
+        showToast("Email verified successfully", "success")
         router.push(CONTINUE_SIGN_UP)
         queryClient.invalidateQueries({ queryKey: ["user"] })
 
@@ -27,18 +29,13 @@ const ValidateEmail = () => {
     },
     onError: (error: any) => {
       const errorMsg = error.response?.data?.description || "An error occurred while verifying OTP"
-      console.log("OTP verification error:", errorMsg)
-      throw new Error(errorMsg)
+      showToast(errorMsg, "error")
     },
   })
 
   const handleValidateEmail = async (payload: VerificationPayload) => {
-    try {
-      await mutation.mutateAsync(payload)
-    } catch (error) {
-      console.log("Mutation error caught:", error)
-      throw error
-    }
+    await mutation.mutateAsync(payload)
+
   }
 
   return { handleValidateEmail, isPending: mutation.isPending, data: mutation.data, error: mutation.error }
