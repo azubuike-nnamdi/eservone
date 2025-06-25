95a8bd1f0ac0e31374b8c35f2ac9934aef64af2c
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-25 09:21:44 +0100

    feat: hook to get message/chat for a room :zap:

---
+++ hooks/query/useGetRoomMessages.ts
@@ -0,0 +1,22 @@
+
+import { chatApi } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetRoomMessages = (groupId: string, receiver: string, sender: string) => {
+  const getRoomMessages = async (groupId: string, receiver: string, sender: string) => {
+    const response = await chatApi.get(`/chat/message?groupId=${groupId}&receiver=${receiver}&sender=${sender}`)
+    return response.data
+  }
+  const { data, isPending, error } = useQuery({
+    queryKey: ['room-messages'],
+    queryFn: () => getRoomMessages(groupId, receiver, sender)
+  })
+
+  return {
+    data,
+    isPending,
+    error
+  }
+}
+
+export default useGetRoomMessages
