bcd2370816e9083f7b81d65ee1a691d4f1af97d6
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-07-01 12:36:27 +0100

    feat: get appointment by user using jwt token :fire:

---
+++ hooks/query/useGetAppointmentByUserId.ts
@@ -0,0 +1,19 @@
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+const useGetAppointmentByUserId = () => {
+
+  const getAppointmentByUserId = async () => {
+    const response = await api.get(`/eserve-one/get-service-appointment-by-service-user`);
+    return response.data;
+  };
+
+  const { data, isPending, error, isError } = useQuery({
+    queryKey: ["appointments"],
+    queryFn: getAppointmentByUserId,
+  });
+
+  return { data, isPending, error, isError };
+};
+
+export default useGetAppointmentByUserId;
