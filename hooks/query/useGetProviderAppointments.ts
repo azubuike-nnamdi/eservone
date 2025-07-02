308427007df3f435b11734f639c5096c89a397ac
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-07-02 10:43:01 +0100

    feat: get provider appointment hook :fire:

---
+++ hooks/query/useGetProviderAppointments.ts
@@ -0,0 +1,24 @@
+import { api } from "@/lib/api";
+import { useAuthStore } from "@/store/auth-store";
+import { useQuery } from "@tanstack/react-query";
+
+const useGetProviderAppointments = () => {
+  const { accessToken } = useAuthStore();
+  const getProviderAppointments = async () => {
+    const response = await api.get(`/eserve-one/get-service-appointment-by-provider`, {
+      headers: {
+        'Authorization': `${accessToken}`,
+      }
+    });
+    return response.data;
+  };
+
+  const { data, isPending, error, isError } = useQuery({
+    queryKey: ["provider-appointments"],
+    queryFn: getProviderAppointments,
+  });
+
+  return { data, isPending, error, isError };
+};
+
+export default useGetProviderAppointments;
