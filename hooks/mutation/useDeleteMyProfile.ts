fd5bd024e5c478d6b57ba083110fed4407ae354d
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-07-22 21:32:45 +0100

    chore: update :zap:

--- hooks/mutation/useDeleteMyProfile.ts
+++ hooks/mutation/useDeleteMyProfile.ts
@@ -1,13 +1,16 @@
 
+import { SIGN_IN } from "@/constants/routes"
 import { AcceptBookingPayload } from "@/constants/types"
 import { useToast } from "@/context/toast-context"
 import { api } from "@/lib/api"
+import { useAuthStore } from "@/store/auth-store"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "expo-router"
 
 const useDeleteMyProfile = () => {
   const router = useRouter()
   const queryClient = useQueryClient()
+  const { clearAuth } = useAuthStore()
 
   const { showToast } = useToast()
 
@@ -16,14 +19,15 @@ const useDeleteMyProfile = () => {
     onSuccess: async (data) => {
       if (data) {
         showToast(data?.data?.description, "success")
-        router.back()
+        clearAuth()
+        router.replace(SIGN_IN)
       }
     },
     onError: (error: { response: { data: { description: string } } }) => {
       showToast(error?.response?.data?.description ?? "Failed to create review", "error")
     },
     onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: ["appointments"] })
+      queryClient.invalidateQueries({ queryKey: ["user"] })
     }
   })
 
--- hooks/query/useGetTransactionQuery.ts
+++ hooks/query/useGetTransactionQuery.ts
@@ -7,12 +7,12 @@ const useGetTransactionQuery = (reference: string) => {
     const response = await api.get(`/eserve-one/verify-payment?reference=${reference}`);
     return response.data;
   };
-  const { data, isPending, error } = useQuery({
+  const { data, isPending, error, refetch } = useQuery({
     queryKey: ["payment", reference],
     queryFn: getTransactionQuery,
   });
 
-  return { data, isPending, error };
+  return { data, isPending, error, refetch };
 }
 
 export default useGetTransactionQuery;
